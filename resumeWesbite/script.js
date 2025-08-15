83d6a965272a147afa415a46b1349510ea0429a2
Misael Amador Gonzalez <98503379+playingdudehd@users.noreply.github.com>
2025-08-14 21:55:05 -0700

    Add files via upload

---
+++ resumeWesbite/script.js
@@ -0,0 +1,4 @@
+document.getElementById('y').textContent = new Date().getFullYear();
+<a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener" class="avatar">
+  <img src="profile.jpg" alt="Misael Amador Gonzalez" />
+</a>

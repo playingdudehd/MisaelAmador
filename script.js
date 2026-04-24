5fa946b159242b8328824427897c11e4c7994b02
Misael Amador Gonzalez <98503379+playingdudehd@users.noreply.github.com>
2026-04-23 22:20:21 -0700

    04232026

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('y').textContent = new Date().getFullYear();
-<a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener" class="avatar">
+<a href="https://www.linkedin.com/in/misa-amador" target="_blank" rel="noopener" class="avatar">
   <img src="profile.jpg" alt="Misael Amador Gonzalez" />
 </a>
